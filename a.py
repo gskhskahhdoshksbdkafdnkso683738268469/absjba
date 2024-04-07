e5ad56dd2db4084047925ff1aed09f7066db5fd2
gskhskahhdoshksbdkafdnkso683738268469 <164631811+gskhskahhdoshksbdkafdnkso683738268469@users.noreply.github.com>
2024-04-07 17:34:54 +0600

    Create a.py

---
+++ a.py
@@ -0,0 +1,29 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests as r
+from flask import Flask
+from threading import Thread
+
+
+ap = Flask(__name__)
+
+@ap.route('/')
+def aaa():
+  return "ji"
+
+
+def aa():
+  ap.run(host="0.0.0.0")
+
+ax = Thread(aa)
+ax.start()
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("6973256484:AAHmY_cTcuzwA6bnFjzUszwb6OESiqJPNdU").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
