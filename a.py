cecc6bcc76de4ac75199adb13fce032e2c140b10
gskhskahhdoshksbdkafdnkso683738268469 <164631811+gskhskahhdoshksbdkafdnkso683738268469@users.noreply.github.com>
2024-04-09 20:32:17 +0600

    Update a.py

--- a.py
+++ a.py
@@ -24,25 +24,19 @@ ax.start()
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
 
-from telebot.async_telebot import AsyncTeleBot
-bot = AsyncTeleBot('6305519973:AAE3EIcBD5HPdAJ0ae5J6vlvsd-9LwMHWsQ')
+def start(update, context):
+    # Define your keyboard buttons
+    keyboard = [['Button 1', 'Button 2'], ['Button 3', 'Button 4']]
 
+    # Create a ReplyKeyboardMarkup object
+    reply_markup = ReplyKeyboardMarkup(keyboard)
 
+    # Send a message with the keyboard
+    update.message.reply_text('Choose a button:', reply_markup=reply_markup)
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
-    await bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+app = ApplicationBuilder().token("6305519973:AAE3EIcBD5HPdAJ0ae5J6vlvsd-9LwMHWsQ").build()
 
+app.add_handler(CommandHandler("start", statt))
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
+app.run_polling()
 
-
-import asyncio
-asyncio.run(bot.polling())
