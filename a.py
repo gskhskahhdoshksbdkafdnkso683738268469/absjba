3df61383453f12aa881094a54aba9b98ff6a18f9
gskhskahhdoshksbdkafdnkso683738268469 <164631811+gskhskahhdoshksbdkafdnkso683738268469@users.noreply.github.com>
2024-04-09 20:13:33 +0600

    Update a.py

--- a.py
+++ a.py
@@ -19,13 +19,30 @@ def aa():
 ax = Thread(target=aa)
 ax.start()
 
+#!/usr/bin/python
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+from telebot.async_telebot import AsyncTeleBot
+bot = AsyncTeleBot('6305519973:AAE3EIcBD5HPdAJ0ae5J6vlvsd-9LwMHWsQ')
 
 
-app = ApplicationBuilder().token("6973256484:AAHmY_cTcuzwA6bnFjzUszwb6OESiqJPNdU").build()
 
-app.add_handler(CommandHandler("hello", hello))
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
 
-app.run_polling()
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+
+import asyncio
+asyncio.run(bot.polling())
